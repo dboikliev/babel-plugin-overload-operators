16e88bb93c6dec75214a495a84318061503b1905
dboikliev <deyanpb@hotmail.com>
2018-09-08 16:13:01 +0300

    Runner.js is also checked with ESlint. Fixed errors.

--- test/runner.js
+++ test/runner.js
@@ -1,23 +1,23 @@
 import assert from 'assert';
-import glob from 'glob'
-import path from 'path'
+import glob from 'glob';
+import path from 'path';
 import fs from 'fs';
 import vm from 'vm';
-import { transformFileSync } from '@babel/core'
-
+import { transformFileSync } from '@babel/core';
+import { describe, it } from 'mocha';
 
 describe('transformation tests', () => {
-    const fixturesDir = 'test/fixtures'
-    const options = JSON.parse(fs.readFileSync('test/options.json')) || {}
-    const fixtures = glob.sync(path.join(fixturesDir, '/*/'))
+    const fixturesDir = 'test/fixtures';
+    const options = JSON.parse(fs.readFileSync('test/options.json')) || {};
+    const fixtures = glob.sync(path.join(fixturesDir, '/*/'));
 
     fixtures.forEach(fixture => {
         const testName = testNameFromFixture(fixture);
 
         testTransformation(fixture, testName, options);
         testExecution(fixture, testName, options);
-    })
-})
+    });
+});
 
 function testTransformation(fixture, testName, options) {
     const expectedPath = path.join(fixture, 'expected.js');
@@ -25,7 +25,7 @@ function testTransformation(fixture, testName, options) {
     if (fs.existsSync(expectedPath) && fs.existsSync(actualPath)) {
         it(`${testName} transforms correctly`, () => {
             const expected = fs.readFileSync(expectedPath, 'utf8')
-                .replace(/\r\n/g, "\n");
+                .replace(/\r\n/g, '\n');
             const actual = transformFileSync(actualPath, options).code;
             assert.strictEqual(actual, expected);
         });
